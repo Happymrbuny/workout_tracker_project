ef70be60e9a05c3246a208bdc6243ed63d30c0c2
Aaron Phelps <phelpsaaronf@gmail.com>
2023-01-23 00:40:04 -0600

    remove ignored files

--- app/workouts/models.py
+++ app/workouts/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
@@ -5,3 +7,4 @@ class Workout(models.Model):
     title = models.CharField(max_length=70, blank=False, default='')
     reps = models.PositiveIntegerField()
     weight = models.PositiveIntegerField()
+    # set_date = models.DateField(default=date.today)
