9299ace2e3a3e0f4fdedc75e7731f16e0db51305
Aaron Phelps <phelpsaaronf@gmail.com>
2023-01-25 23:41:37 -0600

    basic workouts migration

--- app/workouts/models.py
+++ app/workouts/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 # Create your models here.
 class Workout(models.Model):
-    title = models.CharField(max_length=70, blank=False, default='')
+    title = models.CharField(max_length=70, blank=False, default="")
     reps = models.PositiveIntegerField()
     weight = models.PositiveIntegerField()
-    # set_date = models.DateField(default=date.today)
+    setno = models.PositiveIntegerField()
+
--- app/workouts/urls.py
+++ app/workouts/urls.py
@@ -4,9 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    #path('', workouts_views.index, name='home'),
     path('', workouts_views.index.as_view(), name='home'),
     path('api/workouts/', workouts_views.workout_list),
     path('api/workouts/<int:pk>/', workouts_views.workout_detail),
-    # path('api/workouts/published/', workouts_views.workout_list_published)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
